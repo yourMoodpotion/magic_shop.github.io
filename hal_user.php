58240ba4245eaf86fcb8c267c4e439fca4329593
yourMoodpotion <magicshop.bora7@gmail.com>
2023-12-06 01:14:04 +0700

    menambah link membuat newpl

--- hal_user.php
+++ hal_user.php
@@ -5,15 +5,15 @@
         <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header> 
-    <img src="magic.png">
+<header>
+    <img src="magic.png"> 
 </header>
     
 <?php
 session_start();
 if ($_SESSION['level'] == "") {
     header("location: index.php?pesan=gagal");
-    }
+    } 
     ?>
 
     <div class="ket">
@@ -21,12 +21,14 @@ if ($_SESSION['level'] == "") {
     </div>
     <br/>
     <br/>
-    <div class="container"></div>
     <div class="pl">
     <a href="poison.html">Go to Magic Shop</a>
      </div> 
      <div class="spofile">
      <a href="https://open.spotify.com/user/6a2fexpazpygs4tds9tzgjjst?si=e9a3eb80ba424301">My Spofile</a>
+     </div>
+     <div class="newpl">
+        <a href="https://open.spotify.com/playlist/1Oc991XNRuyzTLBm3TP4Tc">Wanna Make a new Poison?</a>
      </div>
 </body>
     </html>
