8aa780ad1767622aedc2f5ccc49b80c080a02efb
yourMoodpotion <magicshop.bora7@gmail.com>
2023-12-06 00:30:48 +0700

    menambah link menuju  spofile

--- hal_user.php
+++ hal_user.php
@@ -14,15 +14,17 @@ if ($_SESSION['level'] == "") {
     ?>
 
     <div class="ket">
-        <div class="img">
-            <image src= "mg.png">
-        </div>
     <h1>Hallo <b><?php echo $_SESSION['username']; ?> </b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</h1>
     </div>
     <br/>
     <br/>
+    <div class="container"></div>
     <div class="pl">
-    <a href="poison.html">Go to Magic Shop</></a>
+    <a href="poison.html">Go to Magic Shop</a>
      </div> 
+     <div class="spofile">
+     <a href="https://open.spotify.com/user/6a2fexpazpygs4tds9tzgjjst?si=e9a3eb80ba424301">My Spofile</a>
+     <img src="mg-removebg-preview.png">
+     </div>
 </body>
     </html>
